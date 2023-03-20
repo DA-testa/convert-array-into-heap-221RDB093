20b0064a29763c6509f6d28bd1c40be93edde25f
221RDB093 <125742217+221RDB093@users.noreply.github.com>
2023-03-20 03:27:37 +0200

    Add files via upload

--- build_heap.py
+++ build_heap.py
@@ -1,33 +1,34 @@
-def heapify(data, n, i, swaps):
+def heapify(data, m, i, swaps):
     minIndex = i
     leftChild = 2*i+1
-    if leftChild < n and data[leftChild] < data[minIndex]:
+    if leftChild < m and data[leftChild] < data[minIndex]:
         minIndex = leftChild
 
     rightChild = 2*i+2
-    if rightChild < n and data[rightChild] < data[minIndex]:
+    if rightChild < m and data[rightChild] < data[minIndex]:
         minIndex = rightChild
 
     if i != minIndex:
         data[i], data[minIndex] = data[minIndex], data[i]
         swaps.append((i, minIndex))
-        heapify(data, n, minIndex, swaps)
+        heapify(data, m, minIndex, swaps)
 
 
 def build_heap(data):
     swaps = []
-    n = len(data)
-    for i in range(n-1, -1, -1):
-        heapify(data, n, i, swaps)
+    m = len(data)
+    for i in range(m-1, -1, -1):
+        heapify(data, m, i, swaps)
 
     return swaps
 
 
 def main():
-    n = int(input())
+    n = input()
+    m = int(n.strip())
     data = list(map(int, input().split()))
 
-    assert len(data) == n
+    #assert len(data) == m
 
     swaps = build_heap(data)
 
